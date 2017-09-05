63fa43aba6c0f5258440509c3c6ee92b17b51edf
Nick Martinez <23nicholasmartinez@gmail.com>
2017-09-05 16:58:35 -0700

    Add equality and comparison tests

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -23,4 +23,34 @@ describe Card do
     # raise unless Card.new(suit: :spades, rank: 4).rank == 4
     raise unless card(rank: 4).rank == 4
   end
+
+  context 'equality' do
+    # def subject
+    #   @subject ||= card(suit: :spades, rank: 4)
+    # end
+    subject { card(suit: :spades, rank: 4) }
+
+    describe 'comparing against self' do
+      # def other
+      #   @other ||= card(suit: :spades, rank: 4)
+      # end
+      let(:other) { card(suit: :spades, rank: 4) }
+
+      it 'is equal' do
+    # it 'is equal to itself' do
+      # subject = card(suit: :spades, rank: 4)
+      # other = card(suit: :spades, rank: 4)
+
+        raise unless subject == other
+      end
+
+      it 'is hash equal' do
+    # it 'is hash equal to itself' do
+      # subject = card(suit: :spades, rank: 4)
+      # other = card(suit: :spades, rank: 4)
+
+        raise unless Set.new([subject, other]).size == 1
+      end
+    end
+  end
 end
