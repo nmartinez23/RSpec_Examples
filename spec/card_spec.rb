37158999b87f81aa3bcc16da2c0ba169314efd99
Nick Martinez <23nicholasmartinez@gmail.com>
2017-09-05 17:02:59 -0700

    Add shared examples test behavior

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -52,5 +52,89 @@ describe Card do
         raise unless Set.new([subject, other]).size == 1
       end
     end
+
+    shared_examples_for 'an unequal card' do
+      it 'is not equal' do
+        raise unless subject != other
+      end
+
+      it 'is not hash equal' do
+        raise unless Set.new([subject, other]).size == 2
+      end
+    end
+
+    describe 'comparing to a card of differing suit' do
+      # def other
+      #   @other ||= card(suit: :hearts, rank: 4)
+      # end
+      let(:other) { card(suit: :hearts, rank: 4) }
+
+      it_behaves_like 'an unequal card'
+    end
+      # it 'is not equal' do
+        # subject = card(suit: :spades, rank: 4)
+        # other = card(suit: :hearts, rank: 4)
+
+        # raise unless subject != other
+      # end
+    # it 'is not equal to a card of differing suit' do
+    #   subject = card(suit: :spades, rank: 4)
+    #   other = card(suit: :hearts, rank: 4)
+
+    #   raise unless subject != other
+    # end
+      # it 'is not hash equal' do
+        # subject = card(suit: :spades, rank: 4)
+        # other = card(suit: :hearts, rank: 4)
+
+        # raise unless Set.new([subject, other]).size == 2
+      # end
+
+    # it 'is not hash equal to a card of differing suit' do
+    #   subject = card(suit: :spades, rank: 4)
+    #   other = card(suit: :hearts, rank: 4)
+
+    #   raise unless Set.new([subject, other]).size == 2
+    # end
+
+    describe 'comparing to a card of differing rank' do
+      # def other
+        # @other ||= card(suit: :spades, rank: 5)
+      # end
+      let(:other) { card(suit: :spades, rank: 5) }
+
+      it_behaves_like 'an unequal card'
+    end
   end
+
+      # it 'is not equal' do
+        # subject = card(suit: :spades, rank: 4)
+        # other = card(suit: :spades, rank: 5)
+
+        # raise unless subject != other
+      # end
+
+    # it 'is not equal to a card of differing rank' do
+    #   subject = card(suit: :spades, rank: 4)
+    #   other = card(suit: :spades, rank: 5)
+
+    #   raise unless subject != other
+    # end
+
+      # it 'is not hash' do
+        # subject = card(suit: :spades, rank: 4)
+        # other = card(suit: :spades, rank: 5)
+
+        # raise unless Set.new([subject, other]).size == 2
+      # end
+    # end
+  # end
+    # it 'is not hash equal to a card of differing rank' do
+    #   subject = card(suit: :spades, rank: 4)
+    #   other = card(suit: :spades, rank: 5)
+
+    #   raise unless Set.new([subject, other]).size == 2
+    # end
+
+
 end
